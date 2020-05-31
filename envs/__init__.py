1297716f3e700a9ce80250a4412fe59a1413d2a5
gtg7784 <gtg7784@naver.com>
2020-05-31 14:01:08 +0900

    udpate env init

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,1 +1,7 @@
 from QlearningAgent.envs.codefairEnv import CodefairEnv
+from gym.envs.registration import register
+
+register(
+  id='CodefairEnv-v0',
+  entry_point='gym_alttp_gridworld.envs:CodefairEnv',
+)
