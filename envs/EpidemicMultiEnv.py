6ee2cae53f51b7c853961694e5ba90546388a98d
Dongyeong Kim <42883224+Dongyeongkim@users.noreply.github.com>
2020-10-23 23:48:28 +0900

    Update EpidemicMultiEnv.py

--- envs/EpidemicMultiEnv.py
+++ envs/EpidemicMultiEnv.py
@@ -31,160 +31,160 @@ AGENT_MATRIX = [
 ]
 
 class EpidemicMultiEnv(gym.Env):
-  def __init__(self, agent_num: int, reward_matrix: np.array) -> None:
-    agent_matrix = AGENT_MATRIX
+    def __init__(self, agent_num: int, reward_matrix: np.array) -> None:
+        agent_matrix = AGENT_MATRIX
 
-    self.agent_num = agent_num
-    self.action_length = 4
-    self.state_length = 4 * 2 * 2
-    self.action_space = spaces.Discrete(self.action_length)
-    self.observation_space = spaces.Discrete(self.state_length)
-    self.rewards = self.min_max_norm(reward_matrix)
+        self.agent_num = agent_num
+        self.action_length = 4
+        self.state_length = 4 * 2 * 2
+        self.action_space = spaces.Discrete(self.action_length)
+        self.observation_space = spaces.Discrete(self.state_length)
+        self.rewards = self.min_max_norm(reward_matrix)
 
-    self.agent_matrix = agent_matrix = [np.array(agent_matrix).astype(int) for _ in range(len(self.rewards))]
-    self.nrow, self.ncol = nrow, ncol = self.agent_matrix[0].shape
-    self.reward_range = (-1, 1)
+        self.agent_matrix = agent_matrix = [np.array(agent_matrix).astype(int) for _ in range(len(self.rewards))]
+        self.nrow, self.ncol = nrow, ncol = self.agent_matrix[0].shape
+        self.reward_range = (-1, 1)
 
-    self.agents = self.get_position(self.agent_num, len(self.rewards))
-    self.has_virus = [False for _ in range(agent_num)]
+        self.agents = self.get_position(self.agent_num, len(self.rewards))
+        self.has_virus = [False for _ in range(agent_num)]
 
-    self.episode = 0
-    self.destinations = self.get_position(agent_num, len(reward_matrix))
-
-  def min_max_norm(self, lst: np.array) -> np.array:
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaler.fit(lst)
-    reward_matrix = scaler.transform(lst)
-
-    return reward_matrix
-
-  def get_position(self, agent_num: int, matrix_num: int) -> list:
-    num_x = list(range(0, self.ncol))
-    num_y = list(range(0, self.nrow))
-    num_z = list(range(0, matrix_num))
-    position_return = list()
-    positions = list()
+        self.episode = 0
+        self.destinations = self.get_position(agent_num, len(reward_matrix))
     
-    for x in num_x:
-      for y in num_y:
-        for z in num_z:
-          positions.append([x, y, z])
+    def min_max_norm(self, lst: np.array) -> np.array:
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        scaler.fit(lst)
+        reward_matrix = scaler.transform(lst)
 
-    for _ in range(agent_num):
-      selected = choice(positions)
-      position_return.append(selected)
-      positions.pop(positions.index(selected))
+        return reward_matrix
     
-    return position_return
+    def get_position(self, agent_num: int, matrix_num: int) -> list:
+        num_x = list(range(0, self.ncol))
+        num_y = list(range(0, self.nrow))
+        num_z = list(range(0, matrix_num))
+        position_return = list()
+        positions = list()
+        
+        for x in num_x:
+        for y in num_y:
+            for z in num_z:
+            positions.append([x, y, z])
 
-  def get_target(self, direction: int, index: int) -> Tuple[int, int]:
-    delta_x = [0, 0, -1, 1]
-    delta_y = [-1, 1, 0, 0]
-    agent_x = self.agents[index][0]
-    agent_y = self.agents[index][1]
-    return agent_x + delta_x[direction], agent_y + delta_y[direction]
+        for _ in range(agent_num):
+        selected = choice(positions)
+        position_return.append(selected)
+        positions.pop(positions.index(selected))
+        
+        return position_return
 
-  def is_virus_around(self, index: int) -> bool:
-    result = False
-    x = self.agents[index][0]
-    y = self.agents[index][1]
-    z = self.agents[index][2]
+    def get_target(self, direction: int, index: int) -> Tuple[int, int]:
+        delta_x = [0, 0, -1, 1]
+        delta_y = [-1, 1, 0, 0]
+        agent_x = self.agents[index][0]
+        agent_y = self.agents[index][1]
+        return agent_x + delta_x[direction], agent_y + delta_y[direction]
 
-    if(x + -1, y):
-      result = True
-    elif(x + 1, y):
-      result = True
-    elif(x, y + -1):
-      result = True
-    elif(x, y + 1):
-      result = True
+    def is_virus_around(self, index: int) -> bool:
+        result = False
+        x = self.agents[index][0]
+        y = self.agents[index][1]
+        z = self.agents[index][2]
 
-    return result
-  
-  def is_move_correct(self, action: int, index: int) -> bool:
-    if(0 < action < 4):
-      agent_x, agent_y = self.get_target(action, index)
-      return (0 <= agent_x < self.nrow - 1) and (0 <= agent_y < self.ncol - 1)
+        if(x + -1, y):
+        result = True
+        elif(x + 1, y):
+        result = True
+        elif(x, y + -1):
+        result = True
+        elif(x, y + 1):
+        result = True
+
+        return result
     
-    return False
-  
-  def encode_state(self, index: int) -> int:
-    return self.has_virus[index] * 4 * (15 ** 2) * 4 * 2 * 2
-
-  def move_link(self, x: int, y: int, z: int, index: int) -> None:
-    agent_x = self.agents[index][0]
-    agent_y = self.agents[index][1]
-    agent_z = self.agents[index][2]
-
-    self.agent_matrix[z][x][y] = 2 # update agent matrix
-
-    self.agent_matrix[agent_z][agent_x][agent_y] = 0
-    self.agents[index][0] = x
-    self.agents[index][1] = y
-    self.agents[index][2] = z
-
-  def move(self, direction: int, index: int) -> Tuple[float, bool]:
-    agent_x = self.agents[index][0]
-    agent_y = self.agents[index][1]
-    z = self.agents[index][2]
-
-    if(agent_x == agent_y == 7): # 역에 있을때
-      z = self.destinations[index][2] # 역을 통해 다른 지역 이동
-
-    x, y = self.get_target(direction, index)
-
-    reward = self.rewards[z][self.episode]
-    object_in_direction = int(self.agent_matrix[z][x][y])
-
-    reward_return = 0
-
-    is_end = False
-
-    if x == self.destinations[index][0] \
-      and y == self.destinations[index][1] \
-      and z == self.destinations[index][2]:
-      is_end = True
-
-    if object_in_direction == EMPTY:
-      self.move_link(x, y, z, index)
-      reward_return = .5
-    elif object_in_direction == VIRUS:
-      self.has_virus = True
-      reward_return = -.1
-    elif object_in_direction == COMMON:
-      reward_return = -.1
-    elif object_in_direction == ISOLATION:
-      self.move_link(x, y, z, index)
-      reward_return = -.1
-
-    reward_return += reward
+    def is_move_correct(self, action: int, index: int) -> bool:
+        if(0 < action < 4):
+        agent_x, agent_y = self.get_target(action, index)
+        return (0 <= agent_x < self.nrow - 1) and (0 <= agent_y < self.ncol - 1)
+        
+        return False
     
-    return reward_return, is_end
+    def encode_state(self, index: int) -> int:
+        return self.has_virus[index] * 4 * (15 ** 2) * 4 * 2 * 2
 
-  def step(self, actions) -> Tuple[list, list, list, Dict]:
-    rewards = list()
-    dones = list()
-    encode_states = list()
-    
-    for i in range(self.agent_num):
-      if (self.is_move_correct(actions[i], i)):
-        r, d = self.move(actions[i], i)
-      else:
-        r, d = -1, False
+    def move_link(self, x: int, y: int, z: int, index: int) -> None:
+        agent_x = self.agents[index][0]
+        agent_y = self.agents[index][1]
+        agent_z = self.agents[index][2]
 
-      encode_states.append(self.encode_state(i))
-      rewards.append(r)
-      dones.append(d)
+        self.agent_matrix[z][x][y] = 2 # update agent matrix
 
-    return encode_states, rewards, dones, {}
+        self.agent_matrix[agent_z][agent_x][agent_y] = 0
+        self.agents[index][0] = x
+        self.agents[index][1] = y
+        self.agents[index][2] = z
 
-  def reset(self) -> int:
-    agent_matrix = AGENT_MATRIX
+    def move(self, direction: int, index: int) -> Tuple[float, bool]:
+        agent_x = self.agents[index][0]
+        agent_y = self.agents[index][1]
+        z = self.agents[index][2]
 
-    self.agent_matrix = agent_matrix = [np.array(agent_matrix).astype(int) for _ in range(len(self.rewards))]
+        if(agent_x == agent_y == 7): # 역에 있을때
+        z = self.destinations[index][2] # 역을 통해 다른 지역 이동
 
-    self.episode += 1
+        x, y = self.get_target(direction, index)
 
-    self.agents = self.get_position(self.agent_num, len(self.rewards))
-    states = [self.encode_state(i) for i in range(self.agent_num)]
-    return states
+        reward = self.rewards[z][self.episode]
+        object_in_direction = int(self.agent_matrix[z][x][y])
+
+        reward_return = 0
+
+        is_end = False
+
+        if x == self.destinations[index][0] \
+        and y == self.destinations[index][1] \
+        and z == self.destinations[index][2]:
+        is_end = True
+
+        if object_in_direction == EMPTY:
+        self.move_link(x, y, z, index)
+        reward_return = .5
+        elif object_in_direction == VIRUS:
+        self.has_virus = True
+        reward_return = -.1
+        elif object_in_direction == COMMON:
+        reward_return = -.1
+        elif object_in_direction == ISOLATION:
+        self.move_link(x, y, z, index)
+        reward_return = -.1
+
+        reward_return += reward
+        
+        return reward_return, is_end
+
+    def step(self, actions) -> Tuple[list, list, list, Dict]:
+        rewards = list()
+        dones = list()
+        encode_states = list()
+        
+        for i in range(self.agent_num):
+        if (self.is_move_correct(actions[i], i)):
+            r, d = self.move(actions[i], i)
+        else:
+            r, d = -1, False
+
+        encode_states.append(self.encode_state(i))
+        rewards.append(r)
+        dones.append(d)
+
+        return encode_states, rewards, dones, {}
+
+    def reset(self) -> int:
+        agent_matrix = AGENT_MATRIX
+
+        self.agent_matrix = agent_matrix = [np.array(agent_matrix).astype(int) for _ in range(len(self.rewards))]
+
+        self.episode += 1
+
+        self.agents = self.get_position(self.agent_num, len(self.rewards))
+        states = [self.encode_state(i) for i in range(self.agent_num)]
+        return states
