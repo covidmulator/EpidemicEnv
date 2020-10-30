d9caf28d2636c1c852cb01e669a038d7a466fc3e
gtg7784 <tae.gun7784@gmail.com>
2020-10-31 08:13:51 +0900

    update

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 import requests
 import json
+import tqdm
 
 with open('./result.json', 'r') as f:
   a = json.load(f)
 
 url = 'http://13.59.81.160:3000/api/matrix'
 
-for item in a:
-  myobj = {'matrix': item, 'type': 'v0'}
+for item in tqdm(a):
+  myobj = {'matrix': item, 'type': 'v1'}
   x = requests.post(url, data = myobj)
